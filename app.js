c00c70a971b414abea404894a16799c8d8555895
Kip Kipper <kip@kipper.me>
2020-12-31 12:16:22 -0800

    New Sylph IP

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const sylph = 'http://47.156.154.41:18934'
+const sylph = 'http://47.156.151.127:18934'
 const plexToken = 'gkZp-GYsCatnMshA7JsM'
 
 var multer = require('multer');
