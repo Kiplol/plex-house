c0464db401226b7975ecb780e69bf9d7b92a9634
Elliott "Kip" Kipper <kip@kipper.me>
2021-08-12 16:31:34 -0700

    New IP

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const sylph = 'http://47.157.93.189:18934'
+const sylph = 'http://47.157.94.64:18934'
 const plexToken = 'gkZp-GYsCatnMshA7JsM'
 
 var multer = require('multer');
